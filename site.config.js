b89b5b76c78e08eb1fbe6afffa88106b035bac17
Ara cho <dkfk2685@naver.com>
2021-11-26 23:55:01 +0900

    Update content for personal branding

--- site.config.js
+++ site.config.js
@@ -1,27 +1,27 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
-
+  rootNotionPageId: '8b99baf29d2e4e76840d6433a1472f12',
+  
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Ara cho',
+  domain: 'Aracho.me',
+  author: 'Ara cho',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  description: '궁금한 걸 참지 않는 개발자',
+  socialImageTitle: 'Ara cho',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  email: null,
-  blog: null,
+  //twitter: 'transitive_bs',
+  //linkedin: 'fisch2',
+  github: 'IceChoco',
+  email: 'dkfk2685@naver.com',
+  blog: 'https://icechoco.github.io/',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
